8c6d88b5d2b6be491ea614eced584e58fc0d130e
Sree <sreelakshmi1002@gmail.com>
2020-07-25 14:25:19 +0530

    add service class for each order

---
+++ BusinessRulesEngine/BusinessRuleEngine/Services/PhysicalProductPayment.cs
@@ -0,0 +1,26 @@
+﻿using BusinessRuleEngine.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessRuleEngine.Services
+{
+    public class PhysicalProductPayment
+    {
+        public PaymentResult ProcessOrder(ProductInfo model)
+        {
+            if (!string.IsNullOrEmpty(model.Name))
+            {
+                return new PaymentResult
+                {
+                    IsSuccess = true,
+                    Message = "Packing slip created for physical product",
+                };
+            }
+            else
+            {
+                throw new InvalidOperationException("Product Name is missing");
+            }
+        }
+    }
+}
---
+++ BusinessRulesEngine/BusinessRuleEngine/Services/VideoPayment.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessRuleEngine.Services
+{
+    public class VideoPayment
+    {
+    }
+}
