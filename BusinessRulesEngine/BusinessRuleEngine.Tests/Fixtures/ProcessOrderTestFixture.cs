c1d6fb5364ef583b552620da81920c3d7f5237e9
Sree <sreelakshmi1002@gmail.com>
2020-07-25 20:26:51 +0530

    Added test case for physical product

---
+++ BusinessRulesEngine/BusinessRuleEngine.Tests/Fixtures/ProcessOrderTestFixture.cs
@@ -0,0 +1,89 @@
+﻿using BusinessRuleEngine.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessRuleEngine.Tests.Fixtures
+{
+    [TestFixture]
+    public class ProcessOrderTestFixture 
+    {
+        List<ProductInfo> products;
+        List<MemeberShipDetails> Members;
+        public ProcessOrderTestFixture()
+        {
+            products = GetProductInfo();
+
+        }
+
+        public List<ProductInfo> GetProductInfo()
+        {
+            List<ProductInfo> details = new List<ProductInfo>();
+            details.Add(new ProductInfo
+            {
+                ProductType = ProductTypes.BOOK,
+                Name = "Book Test 1",
+                Price = 150,
+                Quantity = 5
+
+            });
+
+            details.Add(new ProductInfo
+            {
+                ProductType = ProductTypes.VIDEO,
+                Name = "Video Test 1",
+                Description = "learning to ski",
+                Price = 150,
+                Quantity = 5
+
+            });
+
+            details.Add(new ProductInfo
+            {
+                ProductType = ProductTypes.PHYSICAL_PRODUCT,
+                AgentName = "Agent 001",
+                Commission = 10,
+                Name = "Physical Product Test 1",
+                Description = "learning to ski",
+                Price = 150,
+                Quantity = 5
+
+            });
+            details.Add(new ProductInfo
+            {
+                ProductType = ProductTypes.BOOKORPHYSICAL,
+                AgentName = "Agent 001",
+                Name = "Physical or booking Product Test 1",
+                Price = 150,
+                Quantity = 5
+            });
+
+
+            return details;
+        }
+
+        public List<MemeberShipDetails> GetMembers()
+        {
+            List<MemeberShipDetails> details = new List<MemeberShipDetails>();
+            details.Add(new MemeberShipDetails
+            {
+                MemberName = "User 01",
+                EndDate = DateTime.Today.AddDays(365),
+                StartDate = DateTime.Today,
+                MemberShipType = MemberShipType.ACTIVATION
+            });
+
+            details.Add(new MemeberShipDetails
+            {
+                MemberName = "User 01",
+                EndDate = DateTime.Today.AddDays(365),
+                StartDate = DateTime.Today,
+                MemberShipType = MemberShipType.UPGRADE
+            });
+
+            return details;
+        }
+
+    }
+}
---
+++ BusinessRulesEngine/BusinessRuleEngine.Tests/Services/PhysicalProductTest.cs
@@ -0,0 +1,49 @@
+﻿using BusinessRuleEngine.Common;
+using BusinessRuleEngine.Interfaces;
+using BusinessRuleEngine.Tests.Fixtures;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BusinessRuleEngine.Tests.Services
+{
+    public class PhysicalProductTest: ProcessOrderTestFixture
+    {
+        IProcessOrder OrderProcess;
+
+        [Test]
+        public void When_I_Pass_Valid_PhysicalProduct_Order_it_should_process()
+        {
+            //arrange
+            var physcialProduct = GetProductInfo().Where(x => x.ProductType  == Models.ProductTypes.PHYSICAL_PRODUCT).FirstOrDefault();
+            OrderProcess = ProcessOrders.GetPaymentMethod(Models.PaymentType.PHYSICAL_PRODUCT);
+
+
+            //act
+            var result = OrderProcess.ProcessPayment(physcialProduct);
+
+            //assert
+            Assert.IsTrue(result.IsSuccess);
+            Assert.AreEqual("Packing slip for shipping generated for physical product", result.Message);
+
+
+        }
+
+        [Test]
+        public void ProcessOrder_PhysicalProduct_Name_Empty_Test()
+        {
+
+            //arrange
+            var product = GetProductInfo().Where(x => x.ProductType == Models.ProductTypes.PHYSICAL_PRODUCT).FirstOrDefault();
+            product.Name = string.Empty;
+            OrderProcess = ProcessOrders.GetPaymentMethod(Models.PaymentType.PHYSICAL_PRODUCT);
+
+            //assert
+            var ex = Assert.Throws<InvalidOperationException>(() => OrderProcess.ProcessPayment(product));
+            Assert.That(ex.Message, Is.EqualTo("Physical Product Name is missing"));
+
+        }
+    }
+}
